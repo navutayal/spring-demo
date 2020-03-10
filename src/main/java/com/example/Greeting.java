dc642ec0fd6ab0dc0358aae39700637611d71223
Naveen Gupta <navu.tayal17@gmail.com>
2020-03-10 20:14:32 +0000

    added spring mvc.

--- src/main/java/com/example/Greeting.java
+++ src/main/java/com/example/Greeting.java
@@ -1,9 +1,14 @@
 package com.example;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Greeting {
 
+    @GetMapping("/")
     public String getHello() {
-        return "Hello Gradle Web Application!!";
+        return "My Name is Naveen Gupta";
     }
 
 }
